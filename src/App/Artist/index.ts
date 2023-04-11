bf04e0d9026c4012490416e6195da37baf9f23c5
clementreiffers <reiffersclement@gmail.com>
2023-04-11 22:11:08 +0200

    ajout artiste si existe pas & affichage de tous les artistes avec noms similaires

--- src/App/Artist/index.ts
+++ src/App/Artist/index.ts
@@ -8,15 +8,11 @@ const _artistFactory = (artist: string): ArtistType => R.applySpec({
 })(artist) as ArtistType;
 
 const _computeAddArtist = (artistList: ArtistListType) =>
-	(artist: string): boolean =>
-		R.ifElse(
-			_isArtistExist(artistList),
-			R.F,
-			() => {
-				R.append(_artistFactory(artist), artistList);
-				return true;
-			},
-		)(artist);
+	(artist: string): boolean => R.pipe(
+		_artistFactory,
+		R.tap((artistCreated: ArtistType) => artistList.push(artistCreated)),
+		R.T,
+	)(artist);
 
 const _isArtistExist = (artistList: ArtistListType) =>
 	(artist: string): boolean =>
@@ -26,20 +22,44 @@ const _isArtistExist = (artistList: ArtistListType) =>
 			R.includes(artist),
 		)(artistList);
 
+const _computeAddArtistIfNoExists = (artistList: ArtistListType) =>
+	(artist: string): Response =>
+		R.ifElse(
+			_isArtistExist(artistList),
+			() => okResponse(String(false)),
+			R.pipe(_computeAddArtist(artistList), String, okResponse),
+		)(artist);
+
 const addArtist = (artists: ArtistListType) =>
 	(req: UrlQueryType): Response => R.pipe(
 		decodeURIComponent,
-		_computeAddArtist(artists),
-		String,
-		okResponse,
+		_computeAddArtistIfNoExists(artists),
 	)(req.params.artist);
 
+const _isArtistSearched = (artistSearched: string) =>
+	(artistName: string): boolean =>
+		artistName.toLowerCase().includes(artistSearched.toLowerCase());
+
+// Const getArtist = (artistList: ArtistListType) =>
+// 	(req: UrlQueryType): Response => R.pipe(
+// 		decodeURIComponent,
+// 		(artist: string): string => R.filter(_isArtistSearched(artist), artistList),
+// 		okResponse,
+// 	)(req.params.artist);
+
 const getArtist = (artistList: ArtistListType) =>
-	(req: UrlQueryType): Response => R.pipe(
-		decodeURIComponent,
-		// (artist: string): string[] => R.pluck(artist, artistList),
-		okResponse,
-	)(req.params.artist);
+	(req: UrlQueryType): Response => {
+		const artistFound = [];
+		const artistSearched = decodeURIComponent(req.params.artist);
+		for (const e of artistList) {
+			console.log(e);
+			if (_isArtistSearched(artistSearched)(e.name)) {
+				artistFound.push(e);
+			}
+		}
+
+		return okResponse(JSON.stringify(artistFound));
+	};
 
 const getAllArtists = (artistList: ArtistListType) => (): Response =>
 	R.pipe(
