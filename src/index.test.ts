1539b9da2e066cb4317d7a258461e05e0c33d807
clementreiffers <reiffersclement@gmail.com>
2023-04-14 16:19:10 +0200

    wip

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,12 @@
-import {unstable_dev} from 'wrangler';
 import type {UnstableDevWorker} from 'wrangler';
-import {describe, expect, it, beforeAll, afterAll} from 'vitest';
+import {unstable_dev} from 'wrangler';
+import {afterAll, beforeAll, describe, expect, it} from 'vitest';
 
 describe('Worker', () => {
 	let worker: UnstableDevWorker;
 
 	beforeAll(async () => {
-		worker = await unstable_dev('src/artist.ts', {
+		worker = await unstable_dev('src/index.ts', {
 			experimental: {disableExperimentalWarning: true},
 		});
 	});
@@ -19,7 +19,7 @@ describe('Worker', () => {
 		const resp = await worker.fetch();
 		if (resp) {
 			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot('"Hello World!"');
+			expect(text).toMatchInlineSnapshot('"Not found"');
 		}
 	});
 });
