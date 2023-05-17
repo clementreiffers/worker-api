829e50a39254a005659b12aecb02d4ff9bc1da17
clementreiffers <reiffersclement@gmail.com>
2023-05-17 17:50:39 +0200

    ajout toml personnalisé

--- src/toml-reader/index.js
+++ src/toml-reader/index.js
@@ -1,12 +1,30 @@
 import * as fs from "fs";
 import * as toml from "toml";
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs";
 
-fs.readFile("example.toml", (err, data) => {
-  if (err) throw err;
-  const language = toml.parse(data.toString())?.workerd_config?.lang;
-  if (language) {
-    console.log(language);
-  } else {
-    throw new Error("Language in toml not defined correctly");
-  }
-});
+const manageArgPassed = () => {
+  const argv = yargs(hideBin(process.argv)).argv;
+  if (!argv?.tomlPath) throw new Error("give a toml file to use this script!");
+
+  return argv;
+};
+
+const readTomlFile = ({ tomlPath }) => {
+  fs.readFile(tomlPath, (err, data) => {
+    if (err) throw err;
+    const language = toml.parse(data.toString())?.workerd_config?.lang;
+    if (language) {
+      console.log(language);
+    } else {
+      throw new Error("Language in toml not defined correctly");
+    }
+  });
+};
+
+const main = () => {
+  const data = manageArgPassed();
+  readTomlFile(data);
+};
+
+main();
