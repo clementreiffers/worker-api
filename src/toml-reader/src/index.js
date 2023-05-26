fd6cd38070ff26dcf55ba1c2cb856198254db1e6
clementreiffers <reiffersclement@gmail.com>
2023-05-26 15:31:30 +0200

    update tomlreader

--- src/toml-reader/src/index.js
+++ src/toml-reader/src/index.js
@@ -1,33 +1,20 @@
 import * as toml from 'toml';
-import {exec} from 'child_process';
 import {readFile} from 'fs';
 
-const readTomlFile = ({tomlPath}) => {
+const readTomlFile = ({tomlPath, valuePath}) => {
 	readFile(tomlPath, (err, data) => {
 		if (err) {
 			throw err;
 		}
 
 		const dataParsed = toml.parse(data.toString());
-		const {build} = dataParsed;
-
-		if (build?.container && build?.dir) {
-			// Here we'll directly pull the docker image, execute it, and recuperate all build while they're finished
-			console.log('image:', build.image);
-			console.log('dir:', build.dir);
-			const command = `docker run ${build.container}`;
-			console.log(command);
-		} else if (build.command) {
-			// Here we'll directly execute our generic build image with a command provided by user in the toml file
-
-		} else {
-			throw new Error('build not correctly defined');
-		}
+		const resultat = valuePath.split('.').reduce((obj, key) => obj[key], dataParsed);
+		console.log(resultat);
 	});
 };
 
 const main = () => {
-	readTomlFile({tomlPath: 'wrangler.toml'});
+	readTomlFile({tomlPath: 'wrangler.toml', valuePath: 'build.container'});
 };
 
 main();
